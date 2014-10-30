db8fa8119ce20f02f911a4ce67ad17197819e9b4
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-10-30 14:18:56 +0000

    Initial commit
    |INITIAL COMMIT|
    * Added the bitmap to palette and bitmap to IMAGE build configurations
    * Added Git ignore rules
    * Added GPLv3 [LICENSE] to both projects

---
+++ bmp2img/Source/Source/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int p_Argc, char **p_ppArgv )
+{
+	std::cout << "Bitmap (.BMP) to Wizard IMAGE converter" << std::endl;
+	return 0;
+}
+
---
+++ bmp2pal/Source/Source/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main( int p_Argc, char **p_ppArgv )
+{
+	std::cout << "Bitmap (.BMP) to Wizard palette converter" << std::endl;
+	return 0;
+}
+
